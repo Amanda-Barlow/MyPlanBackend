2d01cdbedbc2b37485c1cb2b712b1c636a5647e4
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-05-31 17:28:56 -0700

    connected to mongodb

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+
 const port = process.env.PORT || 4000;
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
+    
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
+}
 
 const app = express();
 const routes = require('./routes/index')
 const userRoute = require('./routes/userRoute')
 const cors = require("cors");
-const mongoose = require('mongoose')
-const {errorHandler} = require('./middleware/errorMiddleware')
-// const MONGODB_URL = process.env.MONGODB_URL
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/users', userRoute)
 app.use('/api/form', require('./routes/formRoute'))
-
 
 app.use(errorHandler)
 
