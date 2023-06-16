a598131273a5602ed951e8911e01cb72a9c63884
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-06-16 12:33:30 -0800

    debugging nav bar

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -3,8 +3,8 @@ const { planController } = require('../controllers')
 
 router.get('/', planController.getPlan)
 router.post('/', planController.createPlan)
-router.put('/:id', planController.updatePlan)
-router.delete('/:id', planController.deletePlan)
+router.put('/plan/:id', planController.updatePlan)
+router.delete('/plan/:id', planController.deletePlan)
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors("*"))
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({
